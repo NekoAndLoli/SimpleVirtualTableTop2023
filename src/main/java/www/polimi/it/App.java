57f19f8a029923c9010f5436fea85ec086f1f160
Vittorio Xu <vx1245399319@gmail.com>
2023-03-15 13:15:18 +0100

    Initial Commit

---
+++ src/main/java/www/polimi/it/App.java
@@ -0,0 +1,13 @@
+package www.polimi.it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
