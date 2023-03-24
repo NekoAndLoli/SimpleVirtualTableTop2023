29120aa5e55672a4ffa83fe764e296a4cdd68058
Vittorio Xu <vx1245399319@gmail.com>
2023-03-24 01:34:18 +0100

    Pos with javadoc

--- src/main/java/www/polimi/it/Model/Pos.java
+++ src/main/java/www/polimi/it/Model/Pos.java
@@ -4,23 +4,51 @@ public class Pos {
     private Integer x;
     private Integer y;
 
-    public Pos(Integer x, Integer y){
+    /**
+     * Constructor of Position
+     * @param x Integer
+     * @param y Integer
+     * @throws IllegalArgumentException when input is negative
+     */
+    public Pos(Integer x, Integer y)throws IllegalArgumentException{
+        if(x<0)throw new IllegalArgumentException("X negative");
+        if(y<0)throw new IllegalArgumentException("Y negative");
         this.x = x;
         this.y = y;
     }
 
-    public void setX(Integer x) {
+    /**
+     * Sets x
+     * @param x the new Integer to be assigned
+     * @throws IllegalArgumentException when negative
+     */
+    public void setX(Integer x) throws IllegalArgumentException {
+        if(x<0)throw new IllegalArgumentException("X negative");
         this.x = x;
     }
 
-    public void setY(Integer y) {
+    /**
+     * Sets y
+     * @param y the new Integer to be assigned
+     * @throws IllegalArgumentException when negative
+     */
+    public void setY(Integer y) throws IllegalArgumentException {
+        if(y<0)throw new IllegalArgumentException("Y negative");
         this.y = y;
     }
 
+    /**
+     *
+     * @return x Integer
+     */
     public Integer getX() {
         return x;
     }
 
+    /**
+     *
+     * @return y Integer
+     */
     public Integer getY() {
         return y;
     }
