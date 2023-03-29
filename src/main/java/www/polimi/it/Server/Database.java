9077139e732c23b4205a914d5cefe2b6c7adc378
Vittorio Xu <vx1245399319@gmail.com>
2023-03-29 23:52:10 +0200

    Integration of database on uml
    Integration of database on uml, new statements and minor changes

--- src/main/java/www/polimi/it/Server/Database.java
+++ src/main/java/www/polimi/it/Server/Database.java
@@ -1,14 +1,12 @@
 package www.polimi.it.Server;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Database {
     private Connection connection;
-    private String path;
-
-    private HashMap<Statement,PreparedStatement> statements = new HashMap<>();
+    private final String path;
+    private final HashMap<Statement,PreparedStatement> statements = new HashMap<>();
 
     /**
      * Constructor
@@ -18,36 +16,40 @@ public class Database {
         this.path = path;
     }
 
+    /**
+     * Builds up the statements
+     * @throws SQLException if connection is closed or not accepted
+     */
     private void init() throws SQLException {
         statements.put(
                 Statement.GET_MUSIC,
-                connection.prepareStatement(
-                        "SELECT name,location,is_local " +
-                                "FROM Resources JOIN Types " +
-                                "ON Resources.type = Types.type_id " +
-                                "WHERE Types.type_name = \"music\"")
+                connection.prepareStatement(getQuery("music"))
         );
         statements.put(
                 Statement.GET_BG,
-                connection.prepareStatement(
-                        "SELECT name,location,is_local " +
-                                "FROM Resources JOIN Types " +
-                                "ON Resources.type = Types.type_id " +
-                                "WHERE Types.type_name = \"bg\"")
+                connection.prepareStatement(getQuery("bg"))
         );
         statements.put(
                 Statement.GET_TOKEN,
-                connection.prepareStatement(
-                        "SELECT name,location,is_local " +
-                                "FROM Resources JOIN Types " +
-                                "ON Resources.type = Types.type_id " +
-                                "WHERE Types.type_name = \"token\"")
+                connection.prepareStatement(getQuery("token"))
         );
     }
 
     /**
+     * Builds a basic query to select resources
+     * @param string type of the resource to get
+     * @return the query string
+     */
+    private String getQuery(String string){
+        return "SELECT id,name,location,is_local " +
+                "FROM Resources JOIN Types " +
+                "ON Resources.type = Types.type_id " +
+                "WHERE Types.type_name = \"" + string +"\"";
+    }
+
+    /**
      * Should be called before a statement
-     * @return
+     * @return true if the connection is successful
      */
     public boolean connect(){
         String url = "jdbc:sqlite:"+path;
@@ -75,19 +77,40 @@ public class Database {
         }
     }
 
+    /**
+     * gets the token resources
+     * @return a ResultSet containing the tokens in the database
+     * @throws SQLException if error occurs on the query
+     */
     public ResultSet getTokens() throws SQLException {
         return getter(Statement.GET_TOKEN);
     }
 
+    /**
+     * gets the music resources
+     * @return a ResultSet containing the BGM in the database
+     * @throws SQLException if error occurs on the query
+     */
     public ResultSet getMusics() throws SQLException{
         return getter(Statement.GET_MUSIC);
     }
 
+    /**
+     * gets the background resources
+     * @return a ResultSet containing the background images in the database
+     * @throws SQLException if error occurs on the query
+     */
     public ResultSet getBGs() throws SQLException {
         return getter(Statement.GET_BG);
     }
 
-    public ResultSet getter(Statement statement) throws SQLException {
+    /**
+     *
+     * @param statement type of statement to call
+     * @return a ResultSet in base of the statement called
+     * @throws SQLException if error occurs on the query
+     */
+    private ResultSet getter(Statement statement) throws SQLException {
         return statements.get(statement).executeQuery();
     }
 
--- src/main/java/www/polimi/it/Server/Statement.java
+++ src/main/java/www/polimi/it/Server/Statement.java
@@ -3,5 +3,11 @@ package www.polimi.it.Server;
 public enum Statement {
     GET_MUSIC,
     GET_BG,
-    GET_TOKEN
+    GET_TOKEN,
+    INSERT_MUSIC,
+    INSERT_BG,
+    INSERT_TOKEN,
+    GET_GAME_DATA,
+    WRITE_GAME_DATA,
+    DELETE_GAME_DATA
 }
